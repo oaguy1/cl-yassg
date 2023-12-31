25900b11c72f5cf6f53f93bb05eeb12384df3e03
Lily Hughes-Robinson <oaguy1@gmail.com>
2023-12-30 20:50:57 -0500

    Move to tree structure

--- src/cl-yassg.lisp
+++ src/cl-yassg.lisp
@@ -1,15 +1,39 @@
 (in-package #:cl-yassg)
 
-(defun find-markdown-files (pathname)
+(defvar *excluded-dirs* '("assets" "templates"))
+
+(defclass page-node ()
+  ((path
+   :initarg :path
+   :accessor node-path)
+  (is-file-p
+   :initarg :is-file
+   :accessor node-is-file-p)
+  (children
+   :initform '()
+   :accessor node-children)))
+
+(defmethod node-to-list ((node page-node))
+  (let ((lst '()))
+    (dolist (child (node-children node))
+      (push (node-path child) lst)
+      (if (node-children child)
+	  (setf lst (append lst (node-to-list child)))))
+    lst))
+
+
+(defun make-site-tree (path-name &key (include-drafts nil))
   "Find all the markdown files in a given directory, recursively"
-  (let ((filelist '())
-	(files (uiop:directory-files pathname))
-	(dirs (uiop:subdirectories pathname)))
+  (let ((node (make-instance 'page-node :path path-name :is-file nil))
+	(files (uiop:directory-files path-name))
+	(dirs (uiop:subdirectories path-name)))
     (dolist (dir dirs)
-      (setf filelist (append filelist (find-markdown-files dir))))
+      (let ((dirname (first (last (pathname-directory dir)))))
+	(unless (or (member dirname *excluded-dirs* :test #'equal)
+		    (and (string= "drafts" dirname) (not include-drafts)))
+	  (setf (node-children node) (append (node-children node) (list (find-markdown-files dir)))))))
     (dolist (file files)
       (if (string= (pathname-type file) "md")
-	  (push file filelist)))
-    filelist))
-
-
+	  (setf (node-children node)
+		(append (node-children node) (list (make-instance 'page-node :path file :is-file t))))))
+    node))
